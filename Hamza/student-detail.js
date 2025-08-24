b6b778a924d18bf85c4db52ee25dade78c311308
Syed-Hamza13 <syed13hamza@gmail.com>
2025-08-24 23:01:35 +0530

    printing student detail using object

---
+++ Hamza/student-detail.js
@@ -0,0 +1,18 @@
+let student = {
+    name: "Hamza",
+    fatherName: "Syed Mubeen",
+    age: 20,  
+    rollno: "0501CS243D13",  
+    college: "Sagar Institute of Research & Technology Excellence",
+    course: "B.tech",
+    branch: "computer science",
+    Semester: 5,
+    address: "Hoshangabad madhya pradesh 461001",
+    phone: "6265822309",
+    email: "syed13hamza@gmail.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Node js"],
+    hobbies: ["Reading", "Traveling", "Coding", "Gaming"],
+}
+
+console.log("student details:");
+console.log(student);
