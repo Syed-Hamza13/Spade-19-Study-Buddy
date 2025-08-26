62697dfedd7c3f4f852440c1ee9b489101b9aa69
Syed-Hamza13 <syed13hamza@gmail.com>
2025-08-26 23:12:20 +0530

    implement basic HTTP server in minitask-4.js

---
+++ Hamza/minitask-4.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.write("hello world")
+    res.end()
+});
+
+
+server.listen(3000, function(){
+    console.log("Server running on http://localhost:3000")
+}) 
+ 
