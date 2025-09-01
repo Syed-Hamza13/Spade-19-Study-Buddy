272fe18b9ae6296efe0233363970bb7a2361060d
Syed-Hamza13 <syed13hamza@gmail.com>
2025-09-01 23:16:42 +0530

    trying gemini text to speech

---
+++ gemini tts/gemini-tts.js
@@ -0,0 +1,72 @@
+import {GoogleGenAI} from '@google/genai';
+import wav from 'wav';
+
+async function saveWaveFile(
+   filename,
+   pcmData,
+   channels = 1,
+   rate = 24000,
+   sampleWidth = 2,
+) {
+   return new Promise((resolve, reject) => {
+      const writer = new wav.FileWriter(filename, {
+            channels,
+            sampleRate: rate,
+            bitDepth: sampleWidth * 8,
+      });
+
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+
+      writer.write(pcmData);
+      writer.end();
+   });
+}
+
+async function main() {
+   const ai = new GoogleGenAI({apiKey:"AIzaSyA2TytUGOZlHYQbG5rQG7vLd-y0Z00SR6Y"});
+
+   const prompt = `TTS the following conversation between Joe and Jane:
+         Joe: How's it going today Jane?
+         Jane: Not too bad, how about you?
+         Joe: I'm doing well, thanks for asking!
+         Jane: Have you heard about the new project at work?
+         Joe: Yes, it sounds really exciting. I can't wait to get started on it.
+         Jane: Me too! It's going to be a great opportunity for us to learn and grow.`;
+
+   const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash-preview-tts",
+      contents: [{ parts: [{ text: prompt }] }],
+      config: {
+            responseModalities: ['AUDIO'],
+            speechConfig: {
+               multiSpeakerVoiceConfig: {
+                  speakerVoiceConfigs: [
+                        {
+                           speaker: 'Joe',
+                           voiceConfig: {
+                              prebuiltVoiceConfig: { voiceName: 'Pulcherrima ', },
+                              
+                           }
+                           
+                        },
+                        {
+                           speaker: 'Jane',
+                           voiceConfig: {
+                              prebuiltVoiceConfig: { voiceName: 'Charon' }
+                           }
+                        }
+                  ]
+               }
+            }
+      }
+   });
+
+   const data = response.candidates?.[0]?.content?.parts?.[0]?.inlineData?.data;
+   const audioBuffer = Buffer.from(data, 'base64');
+
+   const fileName = 'out.wav';
+   await saveWaveFile(fileName, audioBuffer);
+}
+
+await main();
